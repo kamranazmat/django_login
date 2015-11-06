5ab7762b7591426be1dedfdc7c45e7fbbf9531a3
kamranazmat <azmat.kamran@gmail.com>
2015-11-06 15:43:15 -0500

    its working

--- login/article/views.py
+++ login/article/views.py
@@ -1,59 +1,32 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.template.loader import get_template
-from django.contrib.auth import logout, authenticate, login
+from django.contrib import auth
 from django.template import RequestContext
 from django.template import Context
-from article.models import Article
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 
 # Create your views here.
-def hello(request):
-    name = "Kamran"
-    html = "<html><body>Hi %s, this seems to have worked!</body></html>" % name
-    return HttpResponse(html)
-"""    
-def home(request):
-    t = get_template('main.html')
-    html = t.render(Context())
-    return HttpResponse(html)
-"""    
-def articles(request):
-    language = 'en-us'
-    session_language = 'en-us'
-    
-    if 'lang' in request.COOKIES:
-        language = request.COOKIES['lang']
-        
-    return render_to_response('articles.html',
-                             {'articles' : Article.objects.all(),
-                              'language' : language})
- 
-def language(request, language='en-us'):
-    response = HttpResponse("setting language to %s" % language)
-    response.set_cookie('lang', language)
-    return response
-    
-def home(request):
+   
+def login(request):
     c = {}
     c.update(csrf(request))
-    return render_to_response('main.html', c)
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/welcome')
+    else:
+        return render_to_response('login.html', c)
     
 def auth_view(request):
-    if request.POST:
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = authenticate(username=username, password=password)        
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/welcome')
-            else:
-                return HttpResponseRedirect('/')
-        else:
-            return HttpResponseRedirect('/invalid')
-    return render_to_response('main.html', context_instance=RequestContext(request))
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)        
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/welcome')
+    else:
+        return HttpResponseRedirect('/invalid')
+    #return render_to_response('main.html', context_instance=RequestContext(request))
         
 @login_required(login_url='/login/')        
 def welcome(request):
@@ -65,6 +38,9 @@ def invalid_login(request):
     return render_to_response('invalid_login.html')
  
 def logout(request):
-    logout(request)
+    auth.logout(request)
     return HttpResponseRedirect('/')
+    
+def error(request):
+    return render_to_response('404.html')
     
--- login/login/urls.py
+++ login/login/urls.py
@@ -17,22 +17,25 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.auth.views import login, logout
+from django.conf.urls import handler404
+#from article import views
+#from article.views import error
+#from django.contrib.auth.views import login, logout
 #from article import settings
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/$', 'article.views.hello'),
-    url(r'^home/$', 'article.views.auth_view'),
-    url(r'^$', 'article.views.auth_view'),
-    url(r'^language/$', 'article.views.language'),
-    url(r'^article/$', 'article.views.articles'),
+    #url(r'^hello/$', 'article.views.hello'),
+    url(r'^home/$', 'article.views.login'),
+    url(r'^$', 'article.views.login'),
+    #url(r'^language/$', 'article.views.language'),
+    #url(r'^article/$', 'article.views.articles'),
     #url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'main.html'}),
     #url(r'^home/$', 'django.contrib.auth.views.login', {'template_name': 'main.html'}),
     #url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'main.html'}),
-    url(r'^login/$', 'article.views.auth_view'),
-    #url(r'^auth/$', 'article.views.auth_view'),
+    url(r'^login/$', 'article.views.login'),
+    url(r'^auth/$', 'article.views.auth_view'),
     #url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}),
     url(r'^logout/$', 'article.views.logout'),
     url(r'^welcome/$', 'article.views.welcome'),
@@ -40,3 +43,6 @@ urlpatterns = [
     #url(r'^accounts/login/$', 'article.views.home'),
 ]
 urlpatterns += staticfiles_urlpatterns()
+
+
+handler404 = 'article.views.error'
